1d672476981f4b4dfa3bcdff6e7ffb6701809651
Firnaeth. <gestionprojets.yt@gmail.com>
2026-08-12 13:42:33 +0200

    Update Supabase URL for configuration

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // Configuration Supabase
-const SUPABASE_URL = 'https://lbmpkftnkwsgevsbkbxu.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://lbmpkftnkwsgevsbkbxu.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_-Wx6ublE6921OvFX8MCvwQ_Szhf5kvC'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
